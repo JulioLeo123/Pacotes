0dc947b799f0f3a4769871696a273d7668489202
JulioLeo123 <152197606+JulioLeo123@users.noreply.github.com>
2025-08-22 00:29:19 +0000

    feat(theme): add light/dark theme support and map CSS variables

--- pacotes-web/src/main.jsx
+++ pacotes-web/src/main.jsx
@@ -5,14 +5,17 @@ import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <ThemeProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ThemeProvider>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
